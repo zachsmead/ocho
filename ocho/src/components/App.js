67358f64df53328a4f2f97a6f37e9859fa0ee00e
= <zrsmead@gmail.com>
2019-05-10 16:40:46 -0700

    scaffolding

--- ocho/src/components/App.js
+++ ocho/src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import firebaseConfig from 'config/firebaseConfig.js';
 
 class App extends React.Component {
+  state = {
+    url: '',
+    short: ''
+  }
+
   componentWillMount() {
     firebase.initializeApp(firebaseConfig);
   }
 
+  onURLSubmit = async url => {
+    // shorten the URL and save it in firebase
+
+    this.setState({ short: url }); // change this later to short
+  };
+
   render() {
     return (
-      <div>
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <Input onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
