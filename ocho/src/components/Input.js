95b4f05175fd5de81ded7d1968750ab6d4ef9249
= <zrsmead@gmail.com>
2019-05-10 16:42:43 -0700

    scaffolding

--- ocho/src/components/Input.js
+++ ocho/src/components/Input.js
@@ -14,9 +14,9 @@ class Input extends React.Component {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>enter a URL</label>
             <input
               type="text"
+              placeholder='enter a URL'
               value={this.state.term}
               onChange={e => this.setState({ term: e.target.value })}
             />
