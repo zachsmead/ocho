eeaa2011accda041a9dc4a5cc8d2d5357d398cc6
= <zrsmead@gmail.com>
2019-05-10 23:18:23 -0700

    functions

--- ocho/src/components/Input.js
+++ ocho/src/components/Input.js
@@ -6,15 +6,24 @@ class Input extends React.Component {
   state = { url: '' };
 
   getRandom = () => {
-    const lengths = [6, 6, 7, 7, 8]
-    const length = lengths[Math.floor(Math.random() * lengths.length)];
+    const rand = Math.random(); // generate random number to determine string length
+    const lengths = [
+      { length: 6, min: 0, max: 0.65 }, // setting probabilities of string lengths
+      { length: 7, min: 0.65, max: 0.95 },
+      { length: 8, min: 95, max: 1 }
+    ];
+    const length = lengths.forEach(e => { // get string length
+      if (e.min < rand && rand <= e.max) { // if rand falls in the min-max range
+        return e.length;
+      }
+    });
     var randomString = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     // there are 62 characters to choose from when generating a random string.
     // with 6 character slots, there are 56,800,235,584 (~57 billion) unique combinations.
-    // with 7 character slots, there are 2.1834011e+14 (218,340,110,000,000) unique combinations.
-    // with 8 character slots, there are 2.1834011e+14 (218,340,110,000,000) unique combinations.
+    // with 7 character slots, there are 3.5216146e+12 -- 3,521,614,600,000 (~3.5 trillion) unique combinations.
+    // with 8 character slots, there are 2.1834011e+14 -- 218,340,110,000,000 (~220 trillion) unique combinations.
     console.log(possible.length);
 
     for (var i = 0; i < length; i++)
