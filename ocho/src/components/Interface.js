26a56806968c241fc537099e4c98dd7a3f220b61
= <zrsmead@gmail.com>
2019-05-13 17:44:57 -0700

    refactoring

--- ocho/src/components/Interface.js
+++ ocho/src/components/Interface.js
@@ -22,36 +22,6 @@ class Interface extends React.Component {
     console.log(this.state);
   }
 
-  getRandom = () => {
-    const rand = Math.random(); // generate random number to determine string length
-    const lengths = [
-      { length: 6, min: 0, max: 0.68 }, // setting probabilities of string lengths
-      { length: 7, min: 0.68, max: 0.925 },
-      { length: 8, min: 925, max: 1 }
-    ];
-    var length = 0;
-    lengths.forEach(e => { // determine string length using rand
-      if (rand <= e.max) { // if rand falls in the min-max range
-        length = e.length;
-      }
-    });
-    var randomString = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    // there are 62 characters to choose from when generating a random string.
-    // with 6 character slots, there are 56,800,235,584 (~57 billion) unique combinations.
-    // with 7 character slots, there are 3.5216146e+12 -- 3,521,614,600,000 (~3.5 trillion) unique combinations.
-    // with 8 character slots, there are 2.1834011e+14 -- 218,340,110,000,000 (~220 trillion) unique combinations.
-    console.log(possible.length);
-
-    for (var i = 0; i < length; i++)
-        randomString += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    // return randomString so we can save as a key in firebase.
-    console.log(randomString);
-    return randomString;
-  }
-
   addProtocol = url => {
     var protocolOk = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
 
@@ -100,6 +70,8 @@ class Interface extends React.Component {
   onFormSubmit = event => {
     event.preventDefault(); // prevent page reload
 
+    this.setState({ copied: false });
+
     this.createShortenedURL();
   };
 
@@ -115,7 +87,7 @@ class Interface extends React.Component {
       const urlWithProtocol = this.addProtocol(url);
 
       // generate the short, random id for the url
-      const id = this.getRandom(); // get a random string
+      const id = this.getRandomString(); // get a random string
 
       // make the shortened URL link
       const short = 'ocho.at/' + id;
@@ -171,7 +143,7 @@ class Interface extends React.Component {
     console.log(this.state);
 
     return (
-      <Container>
+      <Container fluid>
         <Row className="" style={{backgroundColor: 'red', flex: 1}}>
           <Col sm="12" className="">
             <form onSubmit={this.onFormSubmit} className="">
---
+++ ocho/src/helpers/getRandomString.js
@@ -0,0 +1,31 @@
+const getRandomString = () => {
+  const rand = Math.random(); // generate random number to determine string length
+  const lengths = [
+    { length: 6, min: 0, max: 0.68 }, // setting probabilities of string lengths
+    { length: 7, min: 0.68, max: 0.925 },
+    { length: 8, min: 925, max: 1 }
+  ];
+  var length = 0;
+  lengths.forEach(e => { // determine string length using rand
+    if (rand <= e.max) { // if rand falls in the min-max range
+      length = e.length;
+    }
+  });
+  var randomString = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // there are 62 characters to choose from when generating a random string.
+  // with 6 character slots, there are 56,800,235,584 (~57 billion) unique combinations.
+  // with 7 character slots, there are 3.5216146e+12 -- 3,521,614,600,000 (~3.5 trillion) unique combinations.
+  // with 8 character slots, there are 2.1834011e+14 -- 218,340,110,000,000 (~220 trillion) unique combinations.
+  console.log(possible.length);
+
+  for (var i = 0; i < length; i++)
+      randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  // return randomString so we can save as a key in firebase.
+  console.log(randomString);
+  return randomString;
+}
+
+export default getRandomString;
